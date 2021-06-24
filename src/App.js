b8f24690ce4d912d888cfe54afb80f81495e72f9
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-06-24 00:27:58 -0400

    Implemented it on the main App starting point.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Navbar from './components/NavBar';
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import Home from './pages';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Home/>
     </Router>
   );
 }
